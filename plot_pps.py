946e97e7e7f5362f1c43afb750ab44f30874da4c
armandcismaru <armandcismaru@gmail.com>
2022-11-29 17:37:07 +0000

    pps plot

--- plot_pps.py
+++ plot_pps.py
@@ -4,8 +4,41 @@ import numpy as np
 
 with open('bse_d001_i05_0001_strats.csv', 'r') as f:
     reader = csv.reader(f)
-    y = []
+    seller_y = []
+    buyer_y = []
+    time = []
+
     for row in reader:
-        y = np.append(y, float(row[0]))
-        print(row[0], row[1])
-        break
+        seller_pps = 0
+        buyer_pps = 0
+        for k, elem in enumerate(row):
+            if 'actvprof' in elem:
+                if k > 212:
+                    seller_pps += float(row[k+1])
+                else:
+                    buyer_pps += float(row[k+1])
+        seller_y = np.append(seller_y, float(seller_pps))
+        buyer_y = np.append(buyer_y, float(buyer_pps))
+        time = np.append(time, float(row[1]))
+
+        print('PPS for all trades at time: ', row[1], 'is: ', seller_pps + buyer_pps)
+        print('PPS for seller trades at time: ', row[1], 'is: ', seller_pps)
+        print('PPS for buyer trades at time: ', row[1], 'is: ', buyer_pps)
+        print('--------------------------------------------')
+
+    time = time / (60*60)
+    fig, ax = plt.subplots()
+    plt.ylabel('Profit per second')
+    plt.xlabel('Time (in hours)') 
+
+    line, = ax.plot(time, seller_y, 'r-')  
+    line.set_label('S-profit')
+
+    line, = ax.plot(time, buyer_y, 'b-')
+    line.set_label('B-profit')
+
+    line, = ax.plot(time, seller_y + buyer_y, 'g-')
+    line.set_label('Total PPS')
+
+    plt.legend()
+    plt.show() 
