5d440cf6a53619ae1f2a44bfe1b77de760478660
armandcismaru <armandcismaru@gmail.com>
2022-12-01 17:03:15 +0000

    changed loop

--- PRDE.py
+++ PRDE.py
@@ -14,7 +14,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--experiment-type", default='default', type=str, help="Type of experiment")
 parser.add_argument("--k-value", default=4, type=int, help="Value of k")
 parser.add_argument("--F-value", default=0.8, type=float, help="Value of F")
-parser.add_argument("--n-days", default=1, type=int, help="Number of days")
+parser.add_argument("--n-days", default=1.0, type=float, help="Number of days")
 
 def plot_trades(trial_id):
     prices_fname = trial_id + '_tape.csv'
@@ -216,10 +216,10 @@ def main(args):
 
     traders_spec = {'sellers': sellers_spec, 'buyers': buyers_spec}
 
-    for fval in [round(i * 0.1, 2) for i in range(21)]: 
-        run_experiments(experiment_type, k_value, fval, n_days, traders_spec)
+    # for fval in [round(i * 0.1, 2) for i in range(21)]: 
+    #     run_experiments(experiment_type, k_value, fval, n_days, traders_spec)
 
-    # run_experiments(experiment_type, k_value, F_value, n_days, traders_spec)
+    run_experiments(experiment_type, k_value, F_value, n_days, traders_spec)
 
 if __name__ == "__main__":
     main(parser.parse_args())
